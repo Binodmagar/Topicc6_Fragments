cd25a506536e611e7ed04c72cfb0dea1d1b7dcad
Binodmagar <ranabinod615@gmail.com>
2019-12-10 08:08:37 +0545

    creating fragment packages and inside that first,second fragment done along with .xml file respectively. Design main_activity.xml file and write code in mainactivity.java file to run.

--- app/src/main/java/com/binod/topicc6_fragments/MainActivity.java
+++ app/src/main/java/com/binod/topicc6_fragments/MainActivity.java
@@ -1,14 +1,55 @@
 package com.binod.topicc6_fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.binod.fragments.FirstFragment;
+import com.binod.fragments.SecondFragments;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnFragments;
+    private Boolean status = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //binding
+        btnFragments = findViewById(R.id.btnFragments);
+
+
+        btnFragments.setOnClickListener(this);
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if(status){
+            FirstFragment firstFragment = new FirstFragment();
+            fragmentTransaction.replace(R.id.FragmentContainer,firstFragment);
+            fragmentTransaction.commit();
+            btnFragments.setText("Load second fragment");
+            status = false;
+        }else{
+            SecondFragments secondFragments = new SecondFragments();
+            fragmentTransaction.replace(R.id.FragmentContainer, secondFragments);
+            fragmentTransaction.commit();
+            btnFragments.setText("Load first fragment");
+            status = true;
+        }
+
+    }
+
+
 }
