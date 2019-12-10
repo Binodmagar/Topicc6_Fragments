0a2fd71b2f3fca487a9fc6d26ed7fc46b6090acd
Binodmagar <ranabinod615@gmail.com>
2019-12-10 08:38:01 +0545

    firstfagments and second fragments backend code to calculate done.

---
+++ app/src/main/java/com/binod/fragments/SecondFragments.java
@@ -0,0 +1,52 @@
+package com.binod.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.binod.topicc6_fragments.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SecondFragments extends Fragment implements View.OnClickListener {
+
+    EditText etRadius;
+    Button btnAreaOfCircle;
+
+
+    public SecondFragments() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_second_fragments, container, false);
+        etRadius = view.findViewById(R.id.etRadius);
+        btnAreaOfCircle = view.findViewById(R.id.btnAreaOfCircle);
+
+        btnAreaOfCircle.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        float radius = Float.parseFloat(etRadius.getText().toString());
+        float area = 3.14f * radius *radius;
+
+        Toast.makeText(getActivity(), "Radius is: " + radius, Toast.LENGTH_SHORT).show();
+
+    }
+}
